87edc2ea9812032bf1cd2b2675d30f35fd57e696
HilalWahyuPriAditya21 <80748383+HilalWahyuPriAditya21@users.noreply.github.com>
2021-04-05 14:22:13 +0700

    Add files via upload

---
+++ Minggu 4/BilanganKelipatanDua.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bilangankelipatandua;
+
+public class BilanganKelipatanDua {
+
+    public static void main(String[] args) {
+         System.out.println("           DO__WHILE        ");
+        System.out.println("Bilangan kelipatan 2 (1-100)");
+        System.out.println("============================");
+        int i = 1;
+        do{
+            System.out.print(i*2 + "   ");
+            i ++;
+        }while(i<=50); 
+    }
+    
+}
